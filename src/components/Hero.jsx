1f4d3197899c149e7b9e2f1bad3167612703c911
HemalDesai <hemald332@gmail.com>
2023-06-08 21:47:56 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,8 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div>
+        <div
+        >
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Hemal</span>
           </h1>
@@ -25,7 +26,7 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* <ComputersCanvas /> */}
+      <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
